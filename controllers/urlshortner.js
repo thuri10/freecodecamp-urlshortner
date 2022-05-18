024d69d0c4708075ff40853fba3148d5fc98bff4
thuri10 <thuri783@gmail.com>
2022-05-18 15:16:20 +0300

    Validate Url during ID generation

--- controllers/urlshortner.js
+++ controllers/urlshortner.js
@@ -2,52 +2,52 @@ const { nanoid } = require("nanoid");
 const fs = require("fs");
 const path = require("path");
 const url = require("url");
+const validUrl = require("valid-url");
 
 //path to store our data parsed
-const filePath = path.join(path.dirname(require.main.filename), "data", "urls.json");
-  
+const filePath = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "urls.json"
+);
+
 exports.Shortener = (req, res, next) => {
-  console.log(req.body);
   let shortUrl = nanoid();
-
-  const responseData = {
-      "original_url": req.body.url,
-      "short_url": shortUrl
+  const url  = req.body.url;
+  if(!validUrl.isWebUri(url)) {
+      res.status(401).json({
+        error: "invalid url"
+      });
+      return
+  } else {
+    const responseData = {
+        original_url: req.body.url,
+        short_url: shortUrl,
+    };
+    fs.writeFile(filePath, JSON.stringify(responseData), (err) => {
+        console.log(err);
+    });
+    res.send(responseData);
   }
-  fs.writeFile(filePath, JSON.stringify(responseData), err => {
-      console.log(err);
-  })
-  res.send(responseData);
-
 };
 
 exports.shortUrlVisit = (req, res, next) => {
-    let myURL;
-    //read array stored of our url
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      data = JSON.parse(data);
-      try {
-        myURL = new URL(data.original_url);
-      } catch (error) {
-        res.json({ error: "invalid url" });
-        return;
-      }
-      if(isValidURL(myURL) === false) {
-            res.json({ error: "invalid url" });
-            return;
-      }
-      if (req.params.short_url === data.short_url) {
-            res.redirect(data.original_url);
-      }    
-    }) ;
-}
-
-
-function isValidURL(string) {
-    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-    return (res !== null)
+  let myURL;
+  //read array stored of our url
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    data = JSON.parse(data);
+    try {
+      myURL = new URL(data.original_url);
+    } catch (error) {
+      res.json({ error: "invalid url" });
+      return;
+    }
+    if (req.params.short_url === data.short_url) {
+      res.redirect(data.original_url);
+    }
+  });
 };
